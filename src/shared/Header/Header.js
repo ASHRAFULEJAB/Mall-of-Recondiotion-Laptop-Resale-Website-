b7118566b9efbab9006c7b5887d43f6714136842
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-11-28 06:24:48 +0600

    header-updated

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -21,19 +21,15 @@ const Header = () => {
             className='inline-flex items-center'
           >
             <svg
-              className='w-8 text-teal-400'
-              viewBox='0 0 24 24'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeMiterlimit='10'
-              stroke='currentColor'
-              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              width='30'
+              height='30'
+              fill='white'
+              className='bi bi-shop'
+              viewBox='0 0 16 16'
             >
-              <rect x='3' y='1' width='7' height='12' />
-              <rect x='3' y='17' width='7' height='6' />
-              <rect x='14' y='1' width='7' height='6' />
-              <rect x='14' y='11' width='7' height='12' />
+              {' '}
+              <path d='M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z' />{' '}
             </svg>
             <span className='ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase'>
               Mall Of Recondition Laptops
@@ -232,16 +228,18 @@ const Header = () => {
                         </Link>
                       </li>
                       {user?.uid ? (
-                        <><li onClick={handleLogout}>
-                        <Link
-                          to='/signin'
-                          className='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none'
-                          aria-label='Sign up'
-                          title='Sign up'
-                        >
-                          Logout
-                        </Link>
-                      </li></>
+                        <>
+                          <li onClick={handleLogout}>
+                            <Link
+                              to='/signin'
+                              className='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none'
+                              aria-label='Sign up'
+                              title='Sign up'
+                            >
+                              Logout
+                            </Link>
+                          </li>
+                        </>
                       ) : (
                         <>
                           <li>
